b3357e7f93a8c19a089e9e0e2c1c4991ea020858
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-05-23 21:08:58 -0400

    Update matrix_addition.cpp

--- matrix_addition.cpp
+++ matrix_addition.cpp
@@ -1,19 +1,22 @@
 #include<iostream> 
 #include<algorithm> 
+#include<vector>
 using namespace std; 
 
 
 class matrix
 {
+	public:
     int rows, col;
-    void matrix(int num_rows, int num_col);
+    
+    matrix(int num_rows, int num_col);
      
-    int m1[rows][col]; 
-    int m2[rows][col]; 
+    vector<vector<int>> m1; 
+    vector<vector<int>> m2; 
 
     void initialize(); 
 
-    int m3[rows][col];
+    vector<vector<int>> m3;
     
     void add(); 
     void display_results(); 
@@ -24,18 +27,19 @@ void matrix::add()
     for(int i=0; i<rows; i++)
         for(int j=0; j<col; j++)
             m3[i][j]=m1[i][j]+m2[i][j];
+    
 }
 
 void matrix:: display_results()
 {
-   cout<<"Matrix 1\n\n"; 
+   cout<<"\nMatrix 1\n\n"; 
 
    for(int i=0; i<rows; i++)
    {
-      cout<<"[";
+      cout<<"[  ";
       for(int j=0; j<col; j++)
       {
-          cout<<m1[i][j]<<" , "; 
+          cout<<m1[i][j]<<"  "; 
       }
 
       cout<<"]\n";
@@ -44,27 +48,27 @@ void matrix:: display_results()
 
 
 
-   cout<<"Matrix 2\n\n";
+   cout<<"\nMatrix 2\n\n";
    for(int i=0; i<rows; i++)
    {
-     cout<<"[";
+     cout<<"[  ";
      for(int j=0; j<col; j++)
      {
-         cout<<m2[i][j]<<" , ";
+         cout<<m2[i][j]<<"  ";
      }
      cout<<"]\n";
   }
 
 
    
-cout<<"Sum Matrix\n\n";
+cout<<"\nSum Matrix\n\n";
    for(int i=0; i<rows; i++)
    {
-     cout<<"[";
+     cout<<"[  ";
      for(int j=0; j< col; j++)
      {
 
-         cout<<m3[i][j]<<" , ";
+         cout<<m3[i][j]<<"  ";
      }
    cout<<"]\n"; 
   } 
@@ -74,24 +78,54 @@ cout<<"Sum Matrix\n\n";
 void matrix::initialize()
 {
 double val= 2;
+	vector<int> vec; 
 
 for(int i=0; i<rows; i++)
+{
+   vec.clear(); 
+   
    for(int j=0; j<col; j++)
-          m1[i][j]=val*2;
+   {
+   	     val*=2; 
+         vec.push_back(val); 
+   }
+   m1.push_back(vec); 
+} 
 
 val=3;
 
 for(int i=0; i<rows; i++)
+{
+	vec.clear(); 
    for(int j=0; j<col; j++)
-         m2[i][j]=val*3; 
+   {
+   	val*=3;
+    vec.push_back(val); 
+   }
+   
+   
+   m2.push_back(vec); 
+}
 
+
+val=0; 
+for(int i=0; i<rows; i++)
+{
+	vec.clear(); 
+	for(int j=0; j<col; j++)
+	{
+		vec.push_back(val); 
+	} 
+	
+   m3.push_back(vec); 
+  } 
 
 }
 
 
 
 
-matrix::matrix(int rows, int columns); 
+matrix::matrix(int num_rows, int num_col)
 {
    rows=num_rows;
    col=num_col;
@@ -100,9 +134,13 @@ matrix::matrix(int rows, int columns);
 
 int main()
 {
-
-
-
+	
+matrix mtx(2,2); 
+	
+mtx.initialize(); 
+mtx.add(); 
+mtx.display_results();
+	
 return 0; 
 
 }
