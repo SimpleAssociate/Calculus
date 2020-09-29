224dd42ba27a255ceae96e0a8f782b7f0a62ae2f
su2675 <30390294+su2675@users.noreply.github.com>
2020-09-29 17:04:46 -0400

    Create calculator.cc

---
+++ calculator.cc
@@ -0,0 +1,41 @@
+#include <iostream> 
+#include <algorithm>
+#include <cmath>
+
+using namespace std; 
+
+
+double sum(double x, double y)
+{
+    return x+y;
+}
+
+double sub(double x, double y)
+{
+    return x-y;
+}
+
+double multiple(double x, double y)
+{
+    return x*y;
+}
+
+double divide(double x, double y)
+{
+    return x/y;
+}
+
+
+
+
+
+int main()
+{
+    
+    double var1;
+    double var2;
+    
+    
+    
+    return 0;
+}
